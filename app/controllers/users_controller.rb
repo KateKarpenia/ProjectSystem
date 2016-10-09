6a82e0a817ce58d1c5d5f5d2fbb8bda19b2797e9
KateKarpenia <katerina.karpenia@gmail.com>
2016-10-09 20:03:38 +0300

    add admin and users actions( edit, update, index, destroy)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :correct_user,   only: [:edit, :update]
+	before_action :admin_user,     only: :destroy
+
 def index
     @user = User.all
   end
@@ -9,12 +13,58 @@ def show
     
   end
   
+def edit
+    @user = User.find(params[:id])
+  end
+  
+def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
 def create
-  @user = User.new(params[:user])
-  user.admin=true
-  @user.save!
-  redirect_to @user
+  @user = User.new(user_params)
+  if @user.save
+  	redirect_to @user
+  else
+  	render 'new'
+  end
+  
 end
+
+def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
+private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, 
+									:password, :password_confirmation, :admin)
+	end
+	
+	def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def current_user?(user)
+    user == current_user
+  end
+  
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :projects
   resources :users
   
-  get 'persons/profile', as: 'user_root'
+  #get 'persons/profile', as: 'user_root'
+  get 'users/:id' => 'user#show'
+  
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
